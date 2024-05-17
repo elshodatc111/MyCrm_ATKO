bb5c3fc126f24a4af11ed5a27dc2af6e72eb2998
elshodatc111 <elshodatc1116@gmail.com>
2024-05-18 01:03:57 +0500

    Save

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\User\PaymeController; 
+use App\Http\Controllers\Api\ApiController; 
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/payme', [PaymeController::class, 'index']);
+
+Route::get('/setting', [ApiController::class, 'setting']);
+Route::post('/setting/update', [ApiController::class, 'update']);
+Route::get('/sms', [ApiController::class, 'smsCount']);
+Route::post('/sms/plus', [ApiController::class, 'smsCountPlus']);
+Route::post('/active', [ApiController::class, 'active']);
+
+Route::get('/cours', [ApiController::class, 'cours']);
